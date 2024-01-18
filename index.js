327e83d47216d84a7de34a4e47543a6d325c0c02
Jaime Rodrigo <40668121+JaRoLoz@users.noreply.github.com>
2024-01-18 01:05:51 +0100

    Reimplemented generation logic

--- index.js
+++ index.js
@@ -1,62 +1,102 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-const keyWords = ["override", "dynamic", "var"];
+const response = await fetch('https://www.npmjs.com/package/@citizenfx/client/file/2f83c0b9a6706e83a1b996af781fb93c14b26fac2491a91325796520377c834a');
+const text = await response.text();
 
-const snakeToPascal = (str) =>
-    str.split("_").map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join("");
-
-const generateFunctionArguments = (params) => {
-    return params.map((param) => {
-        let paramName = keyWords.includes(param.name) ? `_${param.name}` : param.name;
-        return `${paramName}: ${typeToHaxeType(param.type)}`;
-    }).join(", ");
-};
-
-
-const typeToHaxeType = (type) => {
-    switch (type.toLowerCase().replace("*", "")) {
-        case "int":
-            return "Int";
-        case "float":
-            return "Float";
-        case "string":
-            return "String";
-        case "bool":
-            return "Bool";
-        case "object":
-            return "Dynamic";
-        case "char":
-            return "String";
-        case "vector3":
-            return "Vector3";
-        case "void":
-            return "Void";
-        default:
-            return "Int";
+const transformType = (type) => {
+    const replacements = {
+        "number[]": "Vector3",
+        "number": "Number",
+        "string": "String",
+        "boolean": "Boolean",
+        "bool": "Boolean",
+        "any": "Int",
+        "object": "Dynamic",
+        "void": "Void",
     }
+    for (const [key, value] of Object.entries(replacements))
+        if (type.toLowerCase().includes(key))
+            return value;
+    return "Int";
 }
 
-const response = await fetch("https://runtime.fivem.net/doc/natives.json");
-const json = await response.json();
-const declarations = [];
-Object.entries(json).forEach(([nameSpace, natives]) => {
-    Object.entries(natives).forEach(([hash, native]) => {
-        if (!native.name || native.name.startsWith("0x")) return;
-        declarations.push(`static function ${snakeToPascal(native.name)}(${generateFunctionArguments(native.params)}): ${typeToHaxeType(native.results)};`);
-    });
+
+const getFunctionName = (line) => {
+    const splitted = line.split("(");
+    return splitted[0];
+}
+
+const getFunctionReturnType = (line) => {
+    const splitted = line.split(":");
+    const retval = splitted[splitted.length - 1].trim();
+    const returnType = retval.startsWith('[')
+        ? retval.replace("[", "").replace("]", "").trim().split(",")
+        : retval;
+    return returnType;
+}
+
+const generateMultiReturn = (funcName, types) => {
+    const typesStrings = types.map((type, index) => `    var retval${index}:${transformType(type)};`);
+    return `@:multiReturn extern class ${funcName}ReturnType {
+${typesStrings.join("\n")}
+}`;
+}
+
+const getFunctionParameters = (line) => {
+    const splitted = line.split("(");
+    const parameters = splitted[1].split(")")[0].split(",");
+    if (parameters.length === 1 && parameters[0].trim() === "")
+        return [];
+    const paramStrings = parameters.map(param => param.trim());
+    return paramStrings.map(param =>
+        param.trim().split(":"));
+}
+
+const splitted = text.split("declare function");
+const functions = splitted.map(line => line.split(";")[0].trim()).filter(line => !line.startsWith("/*"));
+const multiReturns = [];
+const functionStrings = functions.map(line => {
+    const params = getFunctionParameters(line)
+        .map(([name, type]) => [name, transformType(type)]);
+    const name = getFunctionName(line);
+    const retval = getFunctionReturnType(line);
+    if (Array.isArray(retval)) {
+        multiReturns.push(generateMultiReturn(name, retval));
+        return `static function ${name}(${params.map(([name, type]) => `${name}: ${type}`).join(", ")}):${name}ReturnType;`;
+    } else {
+        return `static function ${name}(${params.map(([name, type]) => `${name}: ${type}`).join(", ")}):${transformType(retval)};`;
+    }
 });
 
-const template = `class Vector3 {
+const template = `@:native("_G")
+extern class Citizen {
+	static var source(default, null):Int;
+	static function Wait(ms:Int):Void;
+	static function CreateThread(callback:Void->Void):Void;
+	static function TriggerEvent(eventName:String, ...args:Dynamic):Void;
+	static function AddEventHandler(eventName:String, cb:(...Dynamic) -> Void):Void;
+	static function TriggerServerEvent(eventName:String, ...args:Dynamic):Void;
+	static function RegisterNetEvent(eventName:String, cb:(...Dynamic) -> Void):Void;
+	static function SendNUIMessage(message:Dynamic):Void;
+	static function SetNuiFocus(hasFocus:Bool, hasCursor:Bool):Void;
+	static function SetNuiFocusKeepInput(keepInput:Bool):Void;
+	static function RegisterNuiCallback(cbName:String, cb:(Dynamic, (Dynamic) -> Void) -> Void):Void;
+}
+
+@:native("_G.json")
+extern class Json {
+	static function decode(json:String):Dynamic;
+	static function encode(value:Dynamic):String;
+}
+
+class Vector3 {
 	extern public var x:Float;
 	extern public var y:Float;
 	extern public var z:Float;
 }
 
-@:native("_G")
-extern class Natives {
-    static function vector3(x:Float, y:Float, z:Float):Vector3;
-    ${declarations.join("\n    ")}
-}
-`;
+${multiReturns.join("\n")}
+
+${functionStrings.join("\n")}`;
 
 console.log(template);
